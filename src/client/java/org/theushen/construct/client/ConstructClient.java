cb7e6ce78e61e0cbf55dc3ce83620ec8c968adcc
TheusHen <mmmatheushenriqueee@gmail.com>
2026-02-18 03:05:24 -0300

    Started Server part, need to implement the TODO, BUILD SUCCESSFUL in 24s

---
+++ src/client/java/org/theushen/construct/client/ConstructClient.java
@@ -0,0 +1,10 @@
+package org.theushen.construct.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ConstructClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
